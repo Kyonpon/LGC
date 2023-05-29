402c8e8070435ef70aef211409e1aed096413105
Kyonpon <kyonpon0503@gmail.com>
2023-05-30 04:17:00 +0800

    DITO NYO ILAGAY YUNG FORMATTED QUESTIONS

---
+++ questions.jsx
@@ -0,0 +1,27 @@
+{
+    question: "What are your primary interests and strengths?", 
+    options: [
+      { text: "Engineering and problem-solving", dept: "COE" }, //syntax {text: "<option 1>", dept: "<COE>"}
+      { text: "Humanities, arts, or social sciences", dept: "CAS" }, //syntax {text: "<option 1>", dept: "<CAS>"}
+      { text: "Natural and health sciences", dept: "CONAH" }, //syntax {text: "<option 1>", dept: "<CONAH>"}
+      {
+        text: "Technological innovation and practical applications", //syntax {text: "<option 1>", dept: "<CTE>"}
+        dept: "CTE",
+      },
+    ],
+  },
+
+  //----------------------------------------------------------------
+
+  {
+    question: "What are your primary interests and strengths?", 
+    options: [
+      { text: "Engineering and problem-solving", dept: "COE" }, //syntax {text: "<option 1>", dept: "<COE>"}
+      { text: "Humanities, arts, or social sciences", dept: "CAS" }, //syntax {text: "<option 1>", dept: "<CAS>"}
+      { text: "Natural and health sciences", dept: "CONAH" }, //syntax {text: "<option 1>", dept: "<CONAH>"}
+      {
+        text: "Technological innovation and practical applications", //syntax {text: "<option 1>", dept: "<CTE>"}
+        dept: "CTE",
+      },
+    ],
+  },
